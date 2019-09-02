a31e597e1baa396a382dfee5f5f53b6561a4d6e5
Diego Lima <diego-dlp@hotmail.com>
2019-09-02 18:00:46 -0300

    Atualização

---
+++ catalogo-filmes/src/app/movies/movies.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+import { MovieDbService } from '../movie-db.service';
+
+@Component({
+  selector: 'app-movies',
+  templateUrl: './movies.component.html',
+  styleUrls: ['./movies.component.sass']
+})
+export class MoviesComponent implements OnInit {
+
+  movies: Array<any>;
+
+  constructor(private moviedbService: MovieDbService) { }
+
+  ngOnInit() {
+    this.getMovie();
+  }
+
+  getMovie() {
+    this.moviedbService.getMovie().subscribe(dados => this.movies = dados);
+  }
+
+}

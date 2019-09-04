57d4c8afac6b93db687d647dc8b472e68bd05a14
Diego Lima <diego-dlp@hotmail.com>
2019-09-04 14:31:40 -0300

    service

--- catalogo-filmes/src/app/components/movie/movie.component.ts
+++ catalogo-filmes/src/app/components/movie/movie.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { MoviedbService } from './../../services/moviedb.service';
 import { TouchSequence } from 'selenium-webdriver';
+import { pluck, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-movie',
@@ -10,19 +11,58 @@ import { TouchSequence } from 'selenium-webdriver';
 })
 export class MovieComponent implements OnInit {
 
-  popular: any;
-  movies: any;
+  populars: any;
+  searchs: any;
+  genres: any;
+  genresMovies: any;
 
   constructor(private moviedbService: MoviedbService) { }
 
   ngOnInit() {
-    this.moviedbService.getMovie()
-      .subscribe(dados => {
-        console.log(dados);
-        this.popular = dados;
 
-        this.movies = this.popular.results;
+    this.moviedbService.populars()
+    .subscribe(({results}: any) => {
+      this.populars = results;
+    });
+
+    this.moviedbService.search('vingadores')
+    .subscribe(({results}: any) => {
+      this.searchs = results;
+    });
+
+    this.moviedbService.genres()
+    .subscribe(({genres}: any) => {
+      this.genres = genres;
+    });
+
+    this.moviedbService.moviesGenre('28')
+    .subscribe(({results}: any) => {
+      this.genresMovies = results;
+    });
+
+
+
+    /*
+
+      this.moviedbService.populars().pipe(
+        pluck("result")
+      )
+      .subscribe(data => {
+        console.log(data);
+        this.populars = data;
       });
+
+      this.moviedbService.populars()
+      .subscribe((data: any) => {
+        this.genres = data.genres;
+      });
+    
+      this.moviedbService.populars()
+      .subscribe(({genres}: any) => {
+        this.genres = genres;
+      });
+
+    */
   }
 
 }
--- catalogo-filmes/src/app/services/moviedb.service.ts
+++ catalogo-filmes/src/app/services/moviedb.service.ts
@@ -8,11 +8,37 @@ import { tap } from 'rxjs/operators';
 })
 export class MoviedbService {
 
-  private url = 'https://api.themoviedb.org/3/movie/popular?api_key=1f572d4eeb91cf40b81c28fd4af4c3e4&language=pt-BR&page=1';
+  private urlMovie = 'https://api.themoviedb.org/3';
+  private apiKey = 'api_key=1f572d4eeb91cf40b81c28fd4af4c3e4';
 
   constructor(private http: HttpClient) { }
 
+
+  private urlTeste = 'https://api.themoviedb.org/3/movie/popular?api_key=1f572d4eeb91cf40b81c28fd4af4c3e4&language=pt-BR&page=1';
   getMovie() {
-    return this.http.get<any[]>(this.url);
+    return this.http.get<any[]>(this.urlTeste);
   }
+
+
+  populars() {
+    const url = `${this.urlMovie}/movie/popular?${this.apiKey}&language=pt-br&page=1`;
+    return this.http.get<any[]>(url);
+  }
+
+  search(query: string) {
+    const url = `${this.urlMovie}/search/movie?${this.apiKey}&language=pt-br&query=${query}&page=1&include_adult=false`;
+    return this.http.get<any[]>(url);
+  }
+
+  genres() {
+    const url = `${this.urlMovie}/genre/movie/list?${this.apiKey}&language=pt-br`;
+    return this.http.get<any[]>(url);
+  }
+
+  moviesGenre(idGenre: string) {
+    // tslint:disable-next-line: max-line-length
+    const url = `${this.urlMovie}/discover/movie?${this.apiKey}&language=pt-BR&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${idGenre}`;
+    return this.http.get<any[]>(url);
+  }
+
 }

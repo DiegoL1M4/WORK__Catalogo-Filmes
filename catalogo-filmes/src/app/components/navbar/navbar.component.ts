f88176d449545e713c7c1b3b1c8f06652ff63a20
Diego Lima <diego-dlp@hotmail.com>
2019-09-11 17:43:35 -0300

    Atualização

--- catalogo-filmes/src/app/components/navbar/navbar.component.ts
+++ catalogo-filmes/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, ɵɵresolveBody } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { MoviedbService } from 'src/app/services/moviedb.service';
@@ -12,7 +12,14 @@ import { Genres } from './../../interfaces/genres';
 export class NavbarComponent implements OnInit {
 
   genres: any;
-  fontTam = 10;
+  fontSize = 10;
+  altoContrate = false;
+
+  public elements = new Array(
+    'body', 'h5', 'h3', 'h2', 'a', 'footer'
+  );
+
+  @ViewChild('estilo', { static: false }) ajuste: ElementRef;
 
   constructor(
     private router$: Router,
@@ -32,5 +39,51 @@ export class NavbarComponent implements OnInit {
   categore(genre: any) {
     this.router$.navigate(['/categories/' + genre.id + '/1', {name: genre.name}]);
   }
+
+  contraste() {
+    this.altoContrate ? this.altoContrate = false : this.altoContrate = true;
+
+    (this.ajuste.nativeElement as HTMLParagraphElement).style.backgroundColor = 'black';
+  }
+
+  fonteAlt(operator: string) {
+    operator === '+' ? this.fontSize++ : this.fontSize--;
+    (this.ajuste.nativeElement as HTMLParagraphElement).style.fontSize = `${this.fontSize}px`;
+  }
+
+
+
+  increaseFont() {
+    this.elements.forEach(e => {
+      this.changeFontSize(e, 'INC');
+    });
+  }
+
+  decreaseFont() {
+    this.elements.forEach(e => {
+      this.changeFontSize(e, 'DEC');
+    });
+  }
+
+  changeFontSize(tag, order) {
+    let elements = document.getElementsByTagName(tag);
+
+    for (let i = 0; i < elements.length; i++) {
+      let element = document.getElementsByTagName(tag)[i];
+      let fontString = window.getComputedStyle(element, null).getPropertyValue('font-size');
+      let fontNumber = parseFloat(fontString);
+
+      if (order === 'INC') {
+        fontNumber++;
+      } else {
+        fontNumber--;
+      }
+
+      element.style.fontSize = fontNumber.toString() + 'px';
+    }
+  }
+
+
+
 
 }
